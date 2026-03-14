1846b129a9818ef45c05309c700a07ad4d0e6a98
Jack Rhoa <jackrhoa@users.noreply.github.com>
2026-03-14 14:21:45 -0400

    Improve roles page

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import UserProfile
@@ -44,9 +45,15 @@ def change_role(request, user_id):
     if not request.user.profile.is_exec():
         return redirect('home')
     if request.method == 'POST':
+        if request.user.id == user_id:
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'ok': False})
+            return redirect('manage_roles')
         profile = get_object_or_404(UserProfile, user__id=user_id)
         new_role = request.POST.get('role')
         if new_role in ['exec', 'member']:
             profile.role = new_role
             profile.save()
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'ok': True, 'role': new_role})
     return redirect('manage_roles')
