c0a907310078cb2c8aeeb58c1edb01b130189073
Jack Rhoa <jackrhoa@users.noreply.github.com>
2026-03-14 20:45:54 -0400

    clean up UI for teams and add max length for team name

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Team(models.Model):
 
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
